0abace81cdf619c72f3ce91a4db63aba780754ea
Charlie Zhang <sunnypig2002@gmail.com>
2020-02-13 17:59:24 +0800

    fix swagger page error

--- settings/base.py
+++ settings/base.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     # 'django.contrib.contenttypes',
     # 'django.contrib.sessions',
     # 'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 
     'corsheaders',
     'django_cleanup',
